c3a287618726f5b8ad2a8a9327529deea1d7aeaa
garystarr-surgi <gary.starr@surgishop.com>
2025-11-03 14:34:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='invoice_lock',
+    version='0.0.1',
+    description='Locks customers with overdue invoices',
+    author='SurgiShop',
+    author_email='gary.starr@surgishop.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=['frappe']
+)
