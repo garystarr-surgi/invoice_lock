68addb72cda90bde53ab8a199787c95ccc19321b
garystarr-surgi <gary.starr@surgishop.com>
2025-11-04 13:34:33 -0500

    Fix customer lock warning - remove indicator and add debug logging
    - Remove indicator property that was causing color error
    - Add console.log statements to debug customer check
    - Match exact pattern from working Frappe Cloud client script

--- invoice_lock/public/js/customer_lock_check.js
+++ invoice_lock/public/js/customer_lock_check.js
@@ -1,47 +1,47 @@
-// Function to check and warn about locked customer
-function check_customer_lock_status(frm, doctype_name) {
-  if (!frm.doc.customer) return;
-
-  frappe.call({
-    method: "frappe.client.get",
-    args: {
-      doctype: "Customer",
-      name: frm.doc.customer
-    },
-    callback: function(r) {
-      if (r.message && r.message.custom_account_locked) {
-        frappe.msgprint({
-          title: __("Customer Locked"),
-          message: __("This customer is locked and cannot be used for {0}.", [doctype_name]),
-          indicator: "red"
-        });
-      }
-    }
-  });
-}
-
 // Handler for Sales Order
 frappe.ui.form.on('Sales Order', {
   customer: function(frm) {
-    check_customer_lock_status(frm, "Sales Orders");
-  },
-  refresh: function(frm) {
-    // Check on form load if customer is already set
-    if (frm.doc.customer) {
-      check_customer_lock_status(frm, "Sales Orders");
-    }
+    if (!frm.doc.customer) return;
+
+    frappe.call({
+      method: "frappe.client.get",
+      args: {
+        doctype: "Customer",
+        name: frm.doc.customer
+      },
+      callback: function(r) {
+        console.log("Sales Order customer check:", r.message);
+        if (r.message && r.message.custom_account_locked) {
+          frappe.msgprint({
+            title: __("Customer Locked"),
+            message: __("This customer is locked and cannot be used for Sales Orders.")
+          });
+        }
+      }
+    });
   }
 });
 
 // Handler for Quotation
 frappe.ui.form.on('Quotation', {
   customer: function(frm) {
-    check_customer_lock_status(frm, "Quotations");
-  },
-  refresh: function(frm) {
-    // Check on form load if customer is already set
-    if (frm.doc.customer) {
-      check_customer_lock_status(frm, "Quotations");
-    }
+    if (!frm.doc.customer) return;
+
+    frappe.call({
+      method: "frappe.client.get",
+      args: {
+        doctype: "Customer",
+        name: frm.doc.customer
+      },
+      callback: function(r) {
+        console.log("Quotation customer check:", r.message);
+        if (r.message && r.message.custom_account_locked) {
+          frappe.msgprint({
+            title: __("Customer Locked"),
+            message: __("This customer is locked and cannot be used for Quotations.")
+          });
+        }
+      }
+    });
   }
 });
