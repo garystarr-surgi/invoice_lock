7a505db14441194a2b6a4cdb1c5a856f48cd95c2
garystarr-surgi <gary.starr@surgishop.com>
2025-11-10 15:52:24 -0500

    Delete invoice_lock/public/js/customer.js

--- invoice_lock/public/js/customer.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on('Customer', {
-    refresh: function(frm) {
-        if (frm.doc.custom_account_locked && frm.doc.custom_locked_status) {
-            frm.fields_dict.custom_locked_status.$wrapper.html(frm.doc.custom_locked_status);
-        }
-    }
-});
